513e794af2761648d6f235ef4c4e88b35eb8eb1d
yllennodsemaj <tuu13154@temple.edu>
2026-02-06 11:22:50 +0900

    added base html pages and images for the website

---
+++ resources/views/website/events.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TUJ Events</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <p>events</p>
+    <br /><br />
+    <h3>Put calendar here</h3>
+    <br /><br />
+    <a href="newEvent.html">Submit a new event here</a>
+  </body>
+</html>
---
+++ resources/views/website/news.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TUJ Club News</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h2>News</h2>
+    <h3>Story one</h3>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam vero
+      consequuntur corrupti delectus repellendus quia magni ullam voluptate?
+      Fugit quos labore officiis necessitatibus inventore, accusamus praesentium
+      magni omnis odit ut?
+    </p>
+  </body>
+</html>

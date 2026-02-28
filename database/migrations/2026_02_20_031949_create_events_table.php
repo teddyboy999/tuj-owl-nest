1a49f12e9a635ca55023d6dc185004fc65464d85
teddyboy999 <haribhushith9999@gmail.com>
2026-03-01 03:46:38 +0900

    more changes on the event migration

--- database/migrations/2026_02_20_031949_create_events_table.php
+++ database/migrations/2026_02_20_031949_create_events_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string("event_organizer_email");
             $table->string("event_affiliation")->nullable(); // club || society || organization
             $table->string("event_category");
+            $table->longText("event_description");
             $table->date("event_date");
             $table->time("start_time");
             $table->time("end_time");
--- resources/views/website/event-list.blade.php
+++ resources/views/website/event-list.blade.php
@@ -7,4 +7,11 @@
     <div id="events">
         <h1>Events</h1>
     </div>
+
+    {{-- TODO: Decide specific colors for different majors / different organizers (society, clubs, private, etc.) --}}
+    {{-- ^^Makes for a uniform design, can set the colors as variables in app.css - like a theme --}}
+    {{-- TODO: Ask Prof. Farid if we can have TAGS as a database element --}}
+
+
+
 @endsection

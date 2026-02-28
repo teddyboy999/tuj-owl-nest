e6137760a235b67092b74e4732bdd876487ca9f6
teddyboy999 <haribhushith9999@gmail.com>
2026-02-28 23:04:12 +0900

    Updated postReply model code

--- app/Models/PostReply.php
+++ app/Models/PostReply.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostReply extends Model
 {
-    //
+    // getReplyByParentPostId($parent_post_id): gets all post replies linked to a single post
+    public function getReplyByParentPostId($parent_post_id)
+    {
+        return $this->where("parent_post_id", $parent_post_id)->get();
+    }
 }

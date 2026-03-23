4e8943939414dc2b291774de496ed8a1821c6c30
teddyboy999 <haribhushith9999@gmail.com>
2026-03-24 06:07:16 +0900

    Individual club pages completed

--- resources/views/website/club-list.blade.php
+++ resources/views/website/club-list.blade.php
@@ -51,10 +51,22 @@
         {{-- Get All Clubs / Organizations --}}
         @foreach ($clubs as $club)
             <article id="card">
-                <img
-                    src="{{ asset('Website_Images/Club_images/TUJ_CS_Society.jpg') }}"
-                    id="club-pic-1"
-                />
+                {{-- Check if the logo exists or not, otherwise, we display the placeholder TUJ logo --}}
+
+                @if (is_null($club->org_logo_url))
+                    <img
+                        src="{{ asset('Website_Images/tuj_logo.png') }}"
+                        alt="placeholder"
+                        id="club-pic-1"
+                    />
+                @elseif (file_exists(public_path($club->org_logo_url)))
+                    <img
+                        src="{{ asset($club->org_logo_url) }}"
+                        alt="Logo"
+                        id="club-pic-1"
+                    />
+                @endif
+
                 <hr />
                 <h1 class="text-center text-2xl font-bold text-black">
                     {{ $club->org_name }}
@@ -64,7 +76,10 @@
                     {{ $club->org_description }}
                 </p>
 
-                <a href="{{ route('clubs.show', ['clubId' => $club->id]) }}">
+                <a
+                    href="{{ route('clubs.show', ['clubId' => $club->id]) }}"
+                    class="m-2 text-center text-blue-700 visited:text-purple-500 hover:text-blue-400 hover:underline"
+                >
                     Show more...
                 </a>
             </article>

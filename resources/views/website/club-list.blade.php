569419483a752b2e1ca87b497b082dae10dd94e8
yllennodsemaj <tuu13154@temple.edu>
2026-03-25 11:17:20 +0900

    fixed submit button and added form validation

--- resources/views/website/club-list.blade.php
+++ resources/views/website/club-list.blade.php
@@ -10,10 +10,18 @@
 
     <div id="club-create">
         <button
-            onclick="window.location.href = '/club-edit'"
+            onclick="window.location.href = '/new-club'"
             class="club-create"
         >
             Create New Club
+        </button>
+    </div>
+    <div id="club-edit">
+        <button
+            onclick="window.location.href = '/club-edit'"
+            class="club-create"
+        >
+            Edit Your Club
         </button>
     </div>
 @endsection

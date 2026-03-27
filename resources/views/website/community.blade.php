2d0749314cb01cd4cb0eb998f5f6fc0b39636e6f
Alonzo Rico <juanlitthatbon@gmail.com>
2026-03-27 10:42:51 +0900

    Added community tab WIP

---
+++ resources/views/website/community.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.web')
+
+@section('content')
+    <div id="top-border">
+        <div id="top-nav-bar"></div>
+    </div>
+    <div id="community" class="fade-in">
+        <p class="p-4 text-3xl font-bold">Community</p>
+    </div>
+@endsection
